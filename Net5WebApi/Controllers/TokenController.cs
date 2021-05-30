fce5526f8bd17f915a4c0dcbc0fccbcff3896c8a
li-rongcheng <Li.Rongcheng@gmail.com>
2021-05-31 00:21:40 +1000

    Update TokenController.cs

--- Net5WebApi/Controllers/TokenController.cs
+++ Net5WebApi/Controllers/TokenController.cs
@@ -1,8 +1,15 @@
 ﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using Net5WebApi.Data;
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace Net5WebApi.Controllers
@@ -11,5 +18,82 @@ namespace Net5WebApi.Controllers
     [ApiController]
     public class TokenController : ControllerBase
     {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly IConfiguration _config;
+
+        public TokenController(ApplicationDbContext context, UserManager<IdentityUser> userManager, IConfiguration config)
+        {
+            _context = context;
+            _userManager = userManager;
+            _config = config;
+        }
+
+        [Route("/token")]
+        [HttpPost]
+        public async Task<IActionResult> Create(string username, string password, string grant_type)
+        {
+            if (await IsValidUsernameAndPassword(username, password))
+            {
+                return new ObjectResult(await GenerateToken(username));
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+
+        private async Task<bool> IsValidUsernameAndPassword(string username, string password)
+        {
+
+            var user = await _userManager.FindByEmailAsync(username);
+            return await _userManager.CheckPasswordAsync(user, password);
+        }
+
+        private async Task<dynamic> GenerateToken(string username)
+        {
+            var user = await _userManager.FindByEmailAsync(username);
+
+            var roles = from userRole in _context.UserRoles
+                        join role in _context.Roles on userRole.RoleId equals role.Id
+                        where userRole.UserId == user.Id
+                        select new { userRole.UserId, userRole.RoleId, role.Name };
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, username ),
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(JwtRegisteredClaimNames.Nbf, new DateTimeOffset(DateTime.Now).ToUnixTimeSeconds().ToString()),
+                new Claim(JwtRegisteredClaimNames.Exp, new DateTimeOffset(DateTime.Now.AddDays(1)).ToUnixTimeSeconds().ToString())
+            };
+
+            foreach (var role in roles)
+            {
+                claims.Add(new Claim(ClaimTypes.Role, role.Name));
+            }
+
+            string key = _config.GetValue<string>("Secrets:SecurityKey");
+
+            var token = new JwtSecurityToken
+                        (
+                            new JwtHeader
+                            (
+                                new SigningCredentials
+                                (
+                                    new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
+                                    SecurityAlgorithms.HmacSha256
+                                )
+                            ),
+                            new JwtPayload(claims)
+                        );
+
+            var output = new
+            {
+                Access_Token = new JwtSecurityTokenHandler().WriteToken(token),
+                UserName = username
+            };
+
+            return output;
+        }
     }
 }
