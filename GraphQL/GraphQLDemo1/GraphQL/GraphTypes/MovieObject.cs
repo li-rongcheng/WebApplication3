9071c7a5ad429405c59623dcf595a8f58dfd4e6a
li-rongcheng <Li.Rongcheng@gmail.com>
2021-06-17 01:26:48 +1000

    add graph types

---
+++ GraphQL/GraphQLDemo1/GraphQL/GraphTypes/MovieObject.cs
@@ -0,0 +1,26 @@
+﻿using GraphQL.Types;
+using GraphQLDemo1.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GraphQLDemo1.GraphQL.GraphTypes
+{
+    public sealed class MovieObject : ObjectGraphType<Movie>
+    {
+        public MovieObject()
+        {
+            Name = nameof(Movie);
+            Description = "A movie in the collection";
+
+            Field(m => m.Id).Description("Identifier of the movie");
+            Field(m => m.Name).Description("Name of the movie");
+            Field(
+                name: "Reviews",
+                description: "Reviews of the movie",
+                type: typeof(ListGraphType<ReviewObject>),
+                resolve: m => m.Source.Reviews);
+        }
+    }
+}
---
+++ GraphQL/GraphQLDemo1/GraphQL/GraphTypes/ReviewObject.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL.Types;
+using GraphQLDemo1.Data.Models;
+
+namespace GraphQLDemo1.GraphQL.GraphTypes
+{
+    public sealed class ReviewObject : ObjectGraphType<Review>
+    {
+        public ReviewObject()
+        {
+            Name = nameof(Review);
+            Description = "A review of the movie";
+
+            Field(r => r.Reviewer).Description("Name of the reviewer");
+            Field(r => r.Stars).Description("Star rating out of five");
+        }
+    }
+}
