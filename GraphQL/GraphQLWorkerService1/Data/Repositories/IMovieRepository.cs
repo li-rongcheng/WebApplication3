dc4e353facc79568cacb7335532f14d6ffc669be
li-rongcheng <Li.Rongcheng@gmail.com>
2021-06-16 16:56:41 +1000

    add repository class

---
+++ GraphQL/GraphQLWorkerService1/Data/Repositories/IMovieRepository.cs
@@ -0,0 +1,40 @@
+﻿using GraphQLWorkerService1.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphQLWorkerService1.Data.Repositories
+{
+    public interface IMovieRepository
+    {
+        Task<Movie> GetMovieByIdAsync(Guid id);
+        Task<Movie> AddReviewToMovieAsync(Guid id, Review review);
+    }
+
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly MovieContext _context;
+
+        public MovieRepository(MovieContext context)
+        {
+            _context = context;
+            _context.Database.EnsureCreated();
+        }
+
+        public Task<Movie> GetMovieByIdAsync(Guid id)
+        {
+            return _context.Movie.Where(m => m.Id == id).AsNoTracking().FirstOrDefaultAsync();
+        }
+
+        public async Task<Movie> AddReviewToMovieAsync(Guid id, Review review)
+        {
+            var movie = await _context.Movie.Where(m => m.Id == id).FirstOrDefaultAsync();
+            movie.AddReview(review);
+            await _context.SaveChangesAsync();
+            return movie;
+        }
+    }
+}
