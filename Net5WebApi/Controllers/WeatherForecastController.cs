38fbb49947fd85c221afb9b80cbe3e594c5817a4
li-rongcheng <Li.Rongcheng@gmail.com>
2021-06-16 16:09:57 +1000

    add graphql worker service project

--- Net5WebApi/Controllers/WeatherForecastController.cs
+++ Net5WebApi/Controllers/WeatherForecastController.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace Net5WebApi.Controllers
 {
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
     [Route("api/[controller]")]
     [ApiController]
     public class WeatherForecastController : ControllerBase
